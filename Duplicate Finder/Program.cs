f02f5232662ddd797ccbf4aefb83d9fbf3b837f6
gbd <gbd@gmx.com>
2015-02-16 18:40:05 -0400

    Logging works

--- Duplicate Finder/Program.cs
+++ Duplicate Finder/Program.cs
@@ -5,12 +5,13 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Gbd.Sandbox.DuplicateFinder.Forms.Forms;
 using NLog;
+using NLog.Config;
 
 namespace Gbd.Sandbox.DuplicateFinder.Forms
 {
     static class Program
     {
-        private static readonly Logger _log = LogManager.GetCurrentClassLogger();    
+        private static readonly Logger _log = LogManager.GetCurrentClassLogger();
 
         /// <summary>
         /// The main entry point for the application.
@@ -18,11 +19,26 @@ namespace Gbd.Sandbox.DuplicateFinder.Forms
         [STAThread]
         static void Main()
         {
+            LogManager.Configuration = new XmlLoggingConfiguration(@"NLog.xml");
+
+            AssertThatNLogIsConfigured();
+
+
             _log.Info("Application starting");
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        private static void AssertThatNLogIsConfigured()
+        {
+            var targets = LogManager.Configuration.AllTargets;
+
+            if (targets.Any(x => x.Name.Equals("AssertTarget")) == false)
+            {
+                throw new Exception("NLog is not configured !!");
+            }
         }
     }
 
