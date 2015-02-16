70a2b508d84a73b4d078ce7d7318f9f229975b8a
gbd <gbd@gmx.com>
2015-02-16 16:04:43 -0400

    Add first logger

--- Duplicate Finder/Program.cs
+++ Duplicate Finder/Program.cs
@@ -4,17 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Gbd.Sandbox.DuplicateFinder.Forms.Forms;
+using NLog;
 
 namespace Gbd.Sandbox.DuplicateFinder.Forms
 {
     static class Program
     {
+        private static readonly Logger _log = LogManager.GetCurrentClassLogger();    
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            _log.Info("Application starting");
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
